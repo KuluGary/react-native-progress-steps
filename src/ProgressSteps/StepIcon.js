2ae83a4d5905eb3a11d407382c489b88e022e03d
Gabriel Cuetara <gcuetara@bexfy.com>
2020-02-17 16:53:12 +0100

    modified

--- src/ProgressSteps/StepIcon.js
+++ src/ProgressSteps/StepIcon.js
@@ -157,8 +157,8 @@ class StepIcon extends Component {
           </Text>
         </View>
         <Text style={styles.labelText}>{this.props.label}</Text>
-        {!this.props.isFirstStep && <View style={styles.leftBar} />}
-        {!this.props.isLastStep && <View style={styles.rightBar} />}
+        {/* {!this.props.isFirstStep && <View style={styles.leftBar} />}
+        {!this.props.isLastStep && <View style={styles.rightBar} />} */}
       </View>
     );
   }
